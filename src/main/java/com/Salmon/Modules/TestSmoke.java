4896c68d4a3e3a08870c1cb6e323ac2069ef2f20
shasa1 <shasalman746@gmail.com>
2021-08-30 00:36:50 +0530

    Working UI test

--- src/main/java/com/Salmon/Modules/TestSmoke.java
+++ src/main/java/com/Salmon/Modules/TestSmoke.java
@@ -1,19 +1,66 @@
 package com.Salmon.Modules;
 
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.security.Provider;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestSmoke {
 
     private WebDriver driver;
     String appURL = "http://google.com";
+    public Logger LOG         = null;
+    static Date s_refDate;
+    protected final String               LOG_FILE    = "console.log";
 
-    @BeforeClass
+    @BeforeSuite
+    public void onTestStart(ITestContext context){
+
+        /**
+         * LOG4J setup
+         */
+
+        // generate log directory
+        String username = System.getProperty( "USERNAME" );
+        String m_logpath = "LogFiles" + File.separator + getReferenceTime("yyyyMM" + File.separator + "yyyyMMddHHmmss") + (StringUtils.isEmpty(username) ? "" : "_" + username) + File.separator + context.getName() + File.separator;
+
+        // create log directory
+        try
+        {
+            new File( m_logpath ).mkdirs();
+        }
+        catch( NullPointerException e )
+        {
+            throw e;
+        }
+
+        // initialize log4j
+        System.setProperty( "LOG_PATH", m_logpath + LOG_FILE );
+        PropertyConfigurator.configure( "src/main/resources/Properties/log4j.properties" );
+
+        // initialize global logger
+        LOG = Logger.getLogger( this.getClass() + m_logpath + LOG_FILE );
+
+        String message ="@BeforeSuite";
+        int asterisk = (110 - message.length()) / 2;
+        String msg = StringUtils.repeat( "*", asterisk ) + message + StringUtils.repeat( "*", asterisk + ( (110 - message.length()) % 2) );
+        LOG.info( msg );
+        LOG.info( "onStart ISuite: " + this.getClass().getName() );
+        LOG.info( "log directory: " + m_logpath+ LOG_FILE );
+    }
+
+    @BeforeTest
     public void testSetUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
@@ -28,9 +75,36 @@ public class TestSmoke {
         Assert.assertEquals(getTitle, "Google");
     }
 
-    @AfterClass
+    @AfterTest
     public void tearDown() {
         driver.quit();
+    }
+
+    /**
+     * With reference to a certain start time
+     * @param format
+     * @return Reference time based from input String format
+     */
+    public static String getReferenceTime( String format )
+    {
+        return getReferenceTime( new SimpleDateFormat( format ) );
+    }
+
+    /**
+     * With reference to a certain start time
+     * @param format
+     * @return Reference time based from input SimpleDateFormat format
+     */
+    public static String getReferenceTime( SimpleDateFormat format )
+    {
+        Date s_date    = new Date();
+        s_refDate = s_date;
+        return getTime( s_date, format );
+    }
+
+    public static String getTime( Date date, SimpleDateFormat format )
+    {
+        return format.format( date );
     }
 
 }
